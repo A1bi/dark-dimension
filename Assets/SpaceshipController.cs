4f9e2847770323d77609f931efa511e28a80856f
Albrecht Oster <a.oster@online.de>
2015-09-07 15:41:50 +0200

    initial commit

---
+++ Assets/SpaceshipController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipController : MonoBehaviour {
+
+	Transform transform;
+
+	// Use this for initialization
+	void Start () {
+		transform = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
